7bfd611985c94aa25a5a4664c9310c9606fa7c85
kumusan <ryogi0200@gmail.com>
2019-08-09 23:42:21 +0900

    ラムダにした

--- src/input.ts
+++ src/input.ts
@@ -1,36 +1,35 @@
 import * as readlineSync from 'readline-sync'
-//　正しく入力されているか
-export function input() {
+// 正しく入力されているか
+export function input(): string {
   for (;;) {
     const read: string = readlineSync.question('number ')
-    if (read.length != 3) {
-      console.log('3つの数字を入力してね！')
+    if (read.length !== 3) {
+      console.error('3つの数字を入力してね！')
       continue
     }
-    if (isNaN(Number(read)) === true) {
-      console.log('数字を入力してね！')
+    if (Number.isNaN(Number(read))) {
+      console.error('数字を入力してね！')
       continue
     }
-    if (duplicate(read) === null) {
-      console.log('同じものは使っちゃだめだよ！')
+    if (duplicate(read)) {
+      console.error('同じものは使っちゃだめだよ！')
       continue
     }
     return read
   }
 }
 
-//重複調べる
-function duplicate(input: string) {
-  const player_input_split: number[] = inputSplit(input)
-  if (
-    player_input_split.filter((x, y, self) => self.indexOf(x) === y).length !==
-    3
-  ) {
-    return null
-  }
-}
+// 重複調べる
+/**
+ *
+ * @param input 入力した値
+ */
+const duplicate = (input: string): boolean =>
+  inputSplit(input).filter((x, y, self) => self.indexOf(x) === y).length !== 3
 
 // 入力した数字を配列にする
-export function inputSplit(n: string): number[] {
-  return n.split('').map(Number)
-}
+/**
+ *
+ * @param n 三桁の数字
+ */
+export const inputSplit = (n: string): number[] => n.split('').map(Number)
