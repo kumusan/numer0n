f9b04822a17af8bc905140032e19c5e007adbfe4
kumusan <ryogi0200@gmail.com>
2019-08-19 09:34:33 +0900

    ラムダ式の修正

--- src/judge.ts
+++ src/judge.ts
@@ -1,6 +1,5 @@
-// eat biteの判断を行う
 /**
- *
+ * eat biteの判断を行う
  * @param com computerの配列
  * @param player playerの配列
  */
@@ -15,29 +14,23 @@ export function judge(com: number[], player: number[]): boolean {
   return false
 }
 
-// eatを返す
 /**
- *
+ * eatを返す
  * @param com computerの配列
  * @param player playerの配列
  */
 function getEat(com: number[], player: number[]): number {
-  const eat: number = com.filter(x => {
-    return player.indexOf(x) === com.indexOf(x)
-  }).length
+  const eat: number = com.filter(x => player.indexOf(x) === com.indexOf(x)).length
   return eat
 }
 
-// biteを返す
 /**
- *
+ * biteを返す
  * @param com computerの配列
  * @param player playerの配列
  * @param eat eatの数
  */
 function getBite(com: number[], player: number[], eat: number): number {
-  const bite: number = com.filter(x => {
-    return player.includes(x)
-  }).length
+  const bite: number = com.filter(x => player.includes(x)).length
   return bite - eat
 }
